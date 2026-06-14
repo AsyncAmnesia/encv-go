f4451bdc3d374361c4d45636b89e9334fe404876
绛亽 <83791825+Soltus@users.noreply.github.com>
2026-06-14 09:30:31 +0800

    Modify linker flags in postprocess.sh

--- app/encv-mobile/scripts/ffmpeg-builder/postprocess.sh
+++ app/encv-mobile/scripts/ffmpeg-builder/postprocess.sh
@@ -112,7 +112,7 @@ link_shared_lib() {
         ${DEPS_INSTALL_DIR}/lib/libmp3lame.a \
         -lm -lz -llog \
         $wl_undef \
-        -Wl,--undefined=ff_af_anull \
+        -Wl,--whole-archive -lavfilter -Wl,--no-whole-archive \
         -Wl,--gc-sections \
         -Wl,--allow-multiple-definition \
         -Wl,--version-script,"$version_script" \
